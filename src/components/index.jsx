64a82f42894107ee26b928e9654db700f332e18a
rony171998 <rony171998@gmail.com>
2022-07-08 19:29:57 -0500

    delete component undefine portfolio

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,6 @@
 export {default as NavBar} from "./Navbar";
 export {default as Footer} from "./Footer";
 export {default as Skills} from "./Skills";
-export {default as Portfolio} from "./Portfolio";
 export {default as Profile} from "./Profile";
 export {default as Contactme} from "./Contactme";
 export {default as Proyects} from "./Proyects";
